148392bec4cb19ce24983b1b6a478949b5436df5
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-07 15:01:32 +0100

    5 done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
 import CardList from "./CardList.js";
-import { robots } from './robots.js';
+
 import SearchBox from "./SearchBox.js";
+import Scroll from "./Scroll.js"
 import './App.css'
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      return response.json()
+    })
+    .then(users =>{
+      this.setState({robots: users})
+    })
+    
   }
 
   onSearchChange = (event) => {
@@ -22,13 +35,20 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
+
+    if(this.state.robots.length === 0){
+      return <h1 className="tc">Loading</h1>
+    } else {
   return (
     <div className="tc">
       <h1 className="f1">RoboFriends</h1>
       <SearchBox searchChange={this.onSearchChange} />
-      <CardList robots={filteredRobots} />
+      <Scroll>  
+        <CardList robots={filteredRobots} />
+      </Scroll>
     </div>
   );
+    }
 }
 }
 
--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ root.render(
   <React.StrictMode>
     <div>
       <App />  
-      
     </div>
     
   </React.StrictMode>
